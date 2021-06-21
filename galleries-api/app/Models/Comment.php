d2728f5f17452490813020a653143809feae9c39
krajko <nikolakrajinovic99@gmail.com>
2021-06-21 19:03:18 +0200

    added models

---
+++ galleries-api/app/Models/Comment.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+use App\Models\User;
+use App\Models\Gallery;
+
+class Comment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'body'
+    ];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function gallery() {
+        return $this->belongsTo(Gallery::class);
+    }
+}
---
+++ galleries-api/app/Models/Gallery.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+use App\Models\User;
+use App\Models\Image;
+use App\Models\Comment;
+
+class Gallery extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description'
+    ];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function images() {
+        return $this->hasMany(Image::class);
+    }
+
+    public function comments() {
+        return $this->hasMany(Comment::class);
+    }
+}
